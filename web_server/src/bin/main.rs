6c251ff957b235aeb9d569fcd928dee6cee361ec
Ryo Suzuki <ryo.s12330@gmail.com>
2023-05-26 11:52:10 +0900

    create multithreds web server

--- web_server/src/bin/main.rs
+++ web_server/src/bin/main.rs
@@ -1,3 +1,6 @@
+extern crate web_server;
+use web_server::ThreadPool;
+
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -7,12 +10,17 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -25,11 +33,12 @@ fn handle_connection(mut stream: TcpStream) {
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
+        thread::sleep(Duration::from_secs(50));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
+
     let mut file = File::open(filename).unwrap();
     let mut contents = String::new();
 
